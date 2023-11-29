ffead8e440b5f3238d852f50e02db83b96b2c88b
Ryan C <65661524+chanrycz@users.noreply.github.com>
2023-11-29 08:58:40 +0800

    Update Dependencies

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { GoogleTranslator } from "@translate-tools/core/translators/GoogleTranslator/index.js";
-import promptSync from 'prompt-sync';
+import promptSync from "prompt-sync";
 
-const prompt = promptSync({sigint: true});
+const prompt = promptSync({ sigint: true });
 
 const translator = new GoogleTranslator({
 	headers: {
@@ -11,128 +11,25 @@ const translator = new GoogleTranslator({
 
 async function main() {
 	console.clear();
-	
-	const langs = [
-		"en",
-		"af",
-		"sq",
-		"am",
-		"ar",
-		"hy",
-		"az",
-		"eu",
-		"be",
-		"bn",
-		"bs",
-		"bg",
-		"ca",
-		"ny",
-		"co",
-		"hr",
-		"cs",
-		"da",
-		"nl",
-		"eo",
-		"et",
-		"tl",
-		"fi",
-		"fr",
-		"fy",
-		"gl",
-		"ka",
-		"de",
-		"el",
-		"gu",
-		"ht",
-		"ha",
-		"hi",
-		"hu",
-		"is",
-		"ig",
-		"id",
-		"ga",
-		"it",
-		"ja",
-		"kn",
-		"kk",
-		"km",
-		"ko",
-		"ku",
-		"ky",
-		"lo",
-		"la",
-		"lv",
-		"lt",
-		"lb",
-		"mk",
-		"mg",
-		"ms",
-		"ml",
-		"mt",
-		"mi",
-		"mr",
-		"mn",
-		"my",
-		"ne",
-		"no",
-		"ps",
-		"fa",
-		"pl",
-		"pt",
-		"pa",
-		"ro",
-		"ru",
-		"sm",
-		"gd",
-		"sr",
-		"st",
-		"sn",
-		"sd",
-		"si",
-		"sk",
-		"sl",
-		"so",
-		"es",
-		"su",
-		"sw",
-		"sv",
-		"tg",
-		"ta",
-		"te",
-		"th",
-		"tr",
-		"uk",
-		"ur",
-		"uz",
-		"vi",
-		"cy",
-		"xh",
-		"yi",
-		"yo",
-		"zu",
-		"zh",
-	];
 
-	var currentTranslation = prompt('Enter phrase to translate: ') || "Your school's AWE wellbeing assessment period ends soon and you haven't yet completed an assessment. Please sign in with you login details at link below to complete the assessment.";
-	
+	const langs = ["en", "af", "sq", "am", "ar", "hy", "az", "eu", "be", "bn", "bs", "bg", "ca", "ny", "co", "hr", "cs", "da", "nl", "eo", "et", "tl", "fi", "fr", "fy", "gl", "ka", "de", "el", "gu", "ht", "ha", "hi", "hu", "is", "ig", "id", "ga", "it", "ja", "kn", "kk", "km", "ko", "ku", "ky", "lo", "la", "lv", "lt", "lb", "mk", "mg", "ms", "ml", "mt", "mi", "mr", "mn", "my", "ne", "no", "ps", "fa", "pl", "pt", "pa", "ro", "ru", "sm", "gd", "sr", "st", "sn", "sd", "si", "sk", "sl", "so", "es", "su", "sw", "sv", "tg", "ta", "te", "th", "tr", "uk", "ur", "uz", "vi", "cy", "xh", "yi", "yo", "zu", "zh"];
+
+	var currentTranslation = prompt("Enter phrase to translate: ") || "Your school's AWE wellbeing assessment period ends soon and you haven't yet completed an assessment. Please sign in with you login details at link below to complete the assessment.";
+
 	console.log(`${langs[0]}: ${currentTranslation}\n`);
-	
+
 	for (let i = 1; i < langs.length; i++) {
-		await translator
-			.translate(currentTranslation, langs[i-1], langs[i])
-			.then(async (translate) => {
-				currentTranslation = translate;
-				
-				await translator
-					.translate(currentTranslation, langs[i], langs[0])
-					.then((back_translate) => {
-						console.log(`${langs[i]}: ${translate}`);
-						console.log(`${langs[0]}: ${back_translate}\n`); // Remove \n if adding countdown
-						
-						// Countdown
-						// console.log(`${i}/${langs.length} (${langs.length - i} left)\n`);
-					});
+		await translator.translate(currentTranslation, langs[i - 1], langs[i]).then(async (translate) => {
+			currentTranslation = translate;
+
+			await translator.translate(currentTranslation, langs[i], langs[0]).then((back_translate) => {
+				console.log(`${langs[i]}: ${translate}`);
+				console.log(`${langs[0]}: ${back_translate}\n`); // Remove \n if adding countdown
+
+				// Countdown
+				// console.log(`${i}/${langs.length} (${langs.length - i} left)\n`);
 			});
+		});
 	}
 }
 
